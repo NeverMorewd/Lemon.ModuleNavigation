c98d3f75c418f6fc02f2c914fa8f6d4310ebbc14
easley <nevermore.wd@me.com>
2024-09-30 18:16:09 +0800

    Add log

---
+++ Lemon.Toolkit/Log/LoggerScope.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lemon.Toolkit.Log
+{
+    public class LoggerScope : IDisposable
+    {
+        private readonly LoggerScope _parent;
+        private readonly string _state;
+
+        public LoggerScope(LoggerScope parent, string state)
+        {
+            _parent = parent;
+            _state = state;
+            Current = this;
+        }
+
+        public static LoggerScope? Current { get; private set; }
+
+        public void Dispose()
+        {
+            Current = _parent;
+        }
+
+        public override string ToString()
+        {
+            return _state;
+        }
+    }
+
+}
---
+++ Lemon.Toolkit/Log/UILoggerProvider.cs
@@ -0,0 +1,35 @@
+﻿using Lemon.Toolkit.Domains;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace Lemon.Toolkit.Log
+{
+    public class UILoggerProvider : ILoggerProvider
+    {
+        private readonly string _name;
+        private readonly IConsoleService _consoleService;
+        private readonly LogLevel _minimumLevel;
+
+        public UILoggerProvider(string name, 
+            IConsoleService consoleService,
+            LogLevel minimumLevel)
+        {
+            _name = name;
+            _consoleService = consoleService;
+            _minimumLevel = minimumLevel;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new UILogger(_name, 
+                categoryName, 
+                _consoleService, 
+                _minimumLevel);
+        }
+
+        public void Dispose()
+        {
+            _consoleService?.Dispose();
+        }
+    }
+}
