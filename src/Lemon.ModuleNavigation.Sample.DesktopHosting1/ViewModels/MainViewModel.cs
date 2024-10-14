c3de4c1df60c93ba7e3a6836faf889d78a19a711
芬里尔吉崽 <nevermore.wd@me.com>
2024-10-14 08:48:34 +0800

    remove absolutes

--- src/Lemon.ModuleNavigation.Sample.DesktopHosting1/ViewModels/MainViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Lemon.ModuleNavigation.Sample.DesktopHosting.ViewModels;
-
-public class MainViewModel : ViewModelBase
-{
-    public string Greeting => "Welcome to Avalonia!";
-}
--- src/Lemon.ModuleNavigation.Sample.DesktopHosting1/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace Lemon.ModuleNavigation.Sample.DesktopHosting.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
