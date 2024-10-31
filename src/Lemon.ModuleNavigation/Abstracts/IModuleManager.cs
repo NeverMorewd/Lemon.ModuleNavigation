4b659721fcff37e58e3ddf2416d08ba3bba75835
Easley <nevermore.wd@me.com>
2024-10-31 18:59:44 +0800

    modulemanager

---
+++ src/Lemon.ModuleNavigation/Abstracts/IModuleManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Lemon.ModuleNavigation.Abstracts
+{
+    public interface IModuleManager
+    {
+        ObservableCollection<IModule> ActiveModules
+        {
+            get;
+            set;
+        }
+    }
+}
