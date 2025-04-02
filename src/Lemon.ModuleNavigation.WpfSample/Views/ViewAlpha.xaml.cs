1f2e472890c68bc992ddee8d4cf6eec90ceca52e
Easley <nevermore.wd@me.com>
2025-04-02 15:45:05 +0800

    wpfdone:code format

--- src/Lemon.ModuleNavigation.WpfSample/Views/ViewAlpha.xaml.cs
+++ src/Lemon.ModuleNavigation.WpfSample/Views/ViewAlpha.xaml.cs
@@ -1,29 +1,15 @@
 ﻿using Lemon.ModuleNavigation.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Lemon.ModuleNavigation.WpfSample.Views
+namespace Lemon.ModuleNavigation.WpfSample.Views;
+
+/// <summary>
+/// Interaction logic for ViewAlpha.xaml
+/// </summary>
+public partial class ViewAlpha : UserControl, IView
 {
-    /// <summary>
-    /// Interaction logic for ViewAlpha.xaml
-    /// </summary>
-    public partial class ViewAlpha : UserControl,IView
+    public ViewAlpha()
     {
-        public ViewAlpha()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
